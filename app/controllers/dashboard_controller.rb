2003616851b1ddc7f61444e02f0382a8d458a23c
Alexander Morrison <1611.developer@gmail.com>
2025-09-13 16:50:26 -0700

    setting up with dashboard w/ project form and list of projects: kind of like twitter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def show
+    @project = Project.new
+    @projects = current_user.projects.order(created_at: :desc)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,52 @@
 class ProjectsController < ApplicationController
+  before_action :require_login
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
+    @project = Project.new
+    @projects = current_user.projects.order(created_at: :desc)
   end
 
   def show
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project created!"
+    else
+      @projects = current_user.projects.order(created_at: :desc)
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to project_path(@project), notice: "Project updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project deleted!"
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
