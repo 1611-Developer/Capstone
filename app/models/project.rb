7affa5038c6547a93d9d343ae518ff391f31c647
Alexander Morrison <1611.developer@gmail.com>
2025-09-17 11:49:00 -0700

    create/edit project validations model and view

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :repository_link, presence: true
 end
