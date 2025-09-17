99723feb720c1444966b57f9c6f7662b1cf3ca80
Alexander Morrison <1611.developer@gmail.com>
2025-09-17 12:53:51 -0700

    Add the show action to the controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by!(username: params[:username])
+    @projects = @user.projects.order(created_at: :desc)
   end
 end
